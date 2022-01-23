05ee4129131729af852c83c30fbdec709b553fd8
mojja7979 <pcs386x@naver.com>
2022-01-24 01:56:30 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
-  const { username, setUsername} = useState('');
-  const { password, setPassword} = useState('')
-  const onSubmit = (event) => {
-    //alert("submit");
-    //submit 같은 경우는 언제나 동작 후 reflash가 동작하는데
-    //preventDefault()로 reflash 동작을 막음
-    event.preventDefault();
-    console.log(password);
-    console.log(username);
-    
-  };
+    const [count, setCount] = useState(0);
+    const [kossie, setKossie] = useState(0);
+    useEffect(() => {
+      console.log('effect')
+      console.log(count);
+    }, [count, kossie])
+    useEffect(() => {
+      console.log('first effect')
+      
+    }, [])
 
+    const increment= () => {
+      setCount(count + 1);
+    };
+  
+    console.log('rendering');
+    
   return (
     <div className="App">
-      <form onSubmit={onSubmit}>
-        <input placeholder="name" 
-          value={username} onChange={(e) => setUsername(e.target.value)}/><br/>
-        <input placeholder="password" 
-        value={password} onChange={(e) => setPassword(e.target.value)}/><br/>
-        <button type="submit">Login</button>
-        </form>
+      <h1>My study!!!!!</h1>
+      <div>{count}</div>
+      <button onClick={increment}>Click</button>
+      <button onClick={() => setKossie(kossie + 1)}>Click1</button>
     </div>
   );
 }
